977982abe1c82503128045f41cde6cea340b895b
Oshanz <spiritualmirror@gmail.com>
2015-04-16 23:54:34 +0530

    CI Rest server for Backbone

---
+++ CI Rest/Working with RESTful Services in CodeIgniter - Tuts+ Code Tutorial_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"8814 http:\/\/net.tutsplus.com\/?p=8814","comments":364}]});
+}
